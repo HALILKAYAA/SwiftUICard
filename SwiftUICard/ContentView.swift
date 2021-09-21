c18b64e4a810d873ef99dc79057deac9d9f08e2d
Halil.KAYA <kayaantalya7@gmail.com>
2021-09-21 21:34:11 +0300

    first commit

---
+++ SwiftUICard/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  SwiftUICard
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    private func getScale(proxy: GeometryProxy) -> CGFloat {
+        var scale: CGFloat = 1
+        
+        let x = proxy.frame(in: .global).minX
+        
+        let diff = abs(x)
+        if diff < 100 {
+            scale = 1 + (100-diff) / 500
+        }
+        
+        return scale
+    }
+     
+    var body: some View {
+        NavigationView{
+            ScrollView{
+                
+                ScrollView(.horizontal) {
+                    HStack(spacing: 50) {
+                        ForEach(0..<20) { num in
+                            GeometryReader { proxy in
+                                
+                                let scale = getScale(proxy: proxy)
+                                
+                                Image("kvp")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 150)
+                                    .clipped()
+                                    .cornerRadius(5)
+                                    .shadow(radius: 5)
+                                    .scaleEffect(CGSize(width: scale, height: scale))
+                                
+//                               Text(scale.description)
+//                                    .font(.system(size:30, weight: .bold))
+                            }
+                            .background(Color.red)
+                            .frame(width: 100, height: 300)
+                        }
+                    }.padding(32)
+                }
+
+            }.navigationTitle("Movies Carousel")
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICard/SwiftUICardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICardApp.swift
+//  SwiftUICard
+//
+//  Created by Halil Kaya on 21.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
